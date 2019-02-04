907b37eee907449f79e6a49c462bd08c8bd96bcd
Isabelle Marduel <isabelle.marduel@gmail.com>
2019-02-04 16:20:09 +0100

    premier envoi

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Form\TaskType;
+
+    /**
+     * Class TaskController
+     * @package App\Controller
+     *
+     * @Route("/")
+     */
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository(Task::class);
+        $tasks = $repository->findBy([], ['id' => 'desc']);
+
+        return $this->render(
+            'index.html.twig',
+            [
+                'tasks' => $tasks
+            ]
+        );
+    }
+
+    /**
+     * {id} est optionnel et doit être un nombre
+     * @Route("/edition/{id}", defaults={"id": null}, requirements={"id": "\d+"})
+     */
+    public function edit(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if (is_null($id)){ // création
+            $task = new Task();
+        } else { // modification
+
+            $task = $em->find(Task::class, $id);
+
+//            404 si l'id reçu dans l'url n'est pas en bdd
+            if (is_null($task)){
+                throw new NotFoundHttpException();
+            }
+        }
+
+//        création du formulaire lié à la tâche
+        $form = $this->createForm(TaskType::class, $task);
+//        le formulaire analyse la requête HTTP et traite le formulaire s'il a été soumis
+        $form->handleRequest($request);
+
+//        si le formulaire a été envoyé et validé
+        if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($task);
+                $em->flush();
+
+//                message de confirmation
+                $this->addFlash('success', 'La tâche a été créée');
+//                redirection vers la liste
+                return $this->redirectToRoute('app_task_index');
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs.');
+            }
+
+        return $this->render(
+            'edit.html.twig',
+            [
+//                passage du formulaire au template
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/suppression/{id}")
+     */
+    public function delete(Task $task)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($task);
+        $em->flush();
+
+        $this->addFlash(
+            'success',
+            'La tâche a été supprimée.'
+        );
+
+        return $this->redirectToRoute(
+            'app_index'
+        );
+    }
+
+
+}
---
+++ src/Form/TaskType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\DBAL\Types\TextType;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => "Nom de la tâche",
+                ]
+                )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
